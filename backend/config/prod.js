1ede3d990f7e7f1b7df53c08db69caa2fa55fef2
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-13 14:35:49 +0800

    backend: initial express structure

---
+++ backend/config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // add production environment secrets/variables here...
+  // secret: process.env.SAMPLE_SECRET,
+};
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Server running...</h1>');
 });
 
+require('./routes/sampleRoutes')(app);
+
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server running...');
 });
