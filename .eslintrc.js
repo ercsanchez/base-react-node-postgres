a11c1116c267f68dfc804233c0533915a2530853
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-13 11:48:56 +0800

    cicd: initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  root: true,
+  extends: ['eslint-config-airbnb-base', 'plugin:node/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['node_modules', 'dist'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'node/no-unsupported-features/es-syntax': [
+      'warn',
+      {
+        version: 'latest',
+        ignores: ['modules'],
+      },
+    ],
+    'node/no-unpublished-require': 'off', // not publishing this app so rule not needed
+    'import/no-extraneous-dependencies': 'warn',
+    // 'import/no-extraneous-dependencies': [
+    //   'warn',
+    //   {
+    //     devDependencies: ['*.config*'], // don't lint matching files
+    //   },
+    // ],
+  },
+};
