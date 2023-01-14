19f5aa23b8ada93adacb2c65d845a1cde3683da4
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-15 06:39:10 +0800

    backend: fix eslint error on importing gitignored file

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod'); // eslint-disable-line global-require
 } else {
-  module.exports = require('./dev'); // eslint-disable-line global-require
+  module.exports = require('./dev'); // eslint-disable-line global-require, import/no-unresolved, import/extensions, node/no-missing-require
 }
